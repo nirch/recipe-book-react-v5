47061d49ae06b8ebd866af40bbcae97f958edce9
nirch <nirh2@yahoo.com>
2021-04-29 10:48:58 +0300

    Controlled Component LoginPage + Submit prints inputs to console

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './LoginPage.css'
 
 function LoginPage(props) {
+    const [email, setEmail] = useState("");
+    const [pwd, setPwd] = useState("");
+
+    function login(e) {
+        e.preventDefault();
+        console.log(email);
+        console.log(pwd);
+    }
+
     return (
         <div className="p-login">
             <h1>Login to Recipe Book</h1>
             <p>or <Link to="/signup">create an account</Link></p>
-            <Form>
+            <Form onSubmit={login}>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" 
+                        value={email} onChange={e => setEmail(e.target.value)} />
                     <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                     </Form.Text>
@@ -19,7 +29,8 @@ function LoginPage(props) {
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" 
+                        value={pwd} onChange={e => setPwd(e.target.value)} />
                 </Form.Group>
                 <Button variant="success" type="submit" block>
                     Login
