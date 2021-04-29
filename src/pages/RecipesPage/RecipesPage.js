de5e6d6a27672be1ad9d0b8397a0d7c5e54e0144
nirch <nirh2@yahoo.com>
2021-04-29 12:13:56 +0300

    adding new recipe button

--- src/pages/RecipesPage/RecipesPage.js
+++ src/pages/RecipesPage/RecipesPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Redirect } from 'react-router';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
 import './RecipesPage.css'
@@ -12,7 +12,11 @@ function RecipesPage({activeUser, recipes}) {
 
     return (
         <Container className="p-recipes">
-            <h1>{activeUser.fname}'s Recipes</h1>
+            <div className="heading">
+                <h1>{activeUser.fname}'s Recipes</h1>
+                <Button variant="link">New Recipe</Button>
+
+            </div>
             <Row>
                 {recipes.map(recipe => 
                     <Col key={recipe.id} md={3} sm={6}>
