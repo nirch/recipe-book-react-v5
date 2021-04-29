6e21a30ae1bbcf3ae10ffd72f063da2db884a3cd
nirch <nirh2@yahoo.com>
2021-04-29 09:43:02 +0300

    adding bootstrap + initial navbar

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import RecipesPage from './pages/RecipesPage/RecipesPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import RecipeNavbar from './components/RecipeNavbar/RecipeNavbar';
+
 
 function App() {
   return (
     <>
       <HashRouter>
         <Switch>
-          <Route exact path="/" ><HomePage/></Route>
+          <Route exact path="/" >
+            <RecipeNavbar/>
+            <HomePage/>
+          </Route>
           <Route exact path="/login" component={LoginPage}></Route>
           <Route exact path="/signup" ><SignupPage/></Route>
-          <Route exact path="/recipes" ><RecipesPage/></Route>
+          <Route exact path="/recipes" >
+            <RecipeNavbar/>
+            <RecipesPage/>
+          </Route>
         </Switch>
       </HashRouter>
     </>
