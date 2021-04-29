4249b63d85bd59f16ceddbf5035cf84d48116fde
nirch <nirh2@yahoo.com>
2021-04-29 12:35:10 +0300

    NewRecipeModal design

--- src/components/NewRecipeModal/NewRecipeModal.js
+++ src/components/NewRecipeModal/NewRecipeModal.js
@@ -1,22 +1,52 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Form, Col, Row } from 'react-bootstrap';
 
-function NewRecipeModal({show, onClose}) {
+function NewRecipeModal({ show, onClose }) {
     return (
-        <Modal show={show} onHide={onClose}>
+        <Modal show={show} onHide={onClose} size="lg">
             <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>New Recipe</Modal.Title>
             </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Body>
+                <Form>
+                    <Form.Group as={Row} controlId="formHorizontalName">
+                        <Form.Label column sm={3}>
+                            Recipe Name
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control type="text" placeholder="Recipe Name" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formHorizontalDesc">
+                        <Form.Label column sm={3}>
+                            Recipe Description
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control type="text" placeholder="Recipe Description" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formHorizontalImg">
+                        <Form.Label column sm={3}>
+                            Recipe Image URL
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control type="text" placeholder="Recipe Image URL" />
+                        </Col>
+                    </Form.Group>
+
+                </Form>
+            </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={onClose}>
-                    Close
+                    Cancel
                 </Button>
                 <Button variant="primary" onClick={onClose}>
-                    Save Changes
+                    Create Recipe
                 </Button>
             </Modal.Footer>
-        </Modal>    
+        </Modal>
     );
 }
 
