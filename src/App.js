5c3c65184a003a7decf85b6c1904fa993803eac5
nirch <nirh2@yahoo.com>
2021-04-29 09:35:37 +0300

    react router + routes for all pages + cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import SignupPage from './pages/SignupPage/SignupPage';
+import RecipesPage from './pages/RecipesPage/RecipesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" ><HomePage/></Route>
+          <Route exact path="/login" component={LoginPage}></Route>
+          <Route exact path="/signup" ><SignupPage/></Route>
+          <Route exact path="/recipes" ><RecipesPage/></Route>
+        </Switch>
+      </HashRouter>
+    </>
   );
 }
 
---
+++ src/pages/RecipesPage/RecipesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RecipesPage(props) {
+    return (
+        <div>
+            RecipesPage
+        </div>
+    );
+}
+
+export default RecipesPage;
