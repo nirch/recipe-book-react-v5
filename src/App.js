cc33cf81901a680835bdb91f01ebbec75599d0c6
nirch <nirh2@yahoo.com>
2021-04-29 09:56:53 +0300

    activeUser state in App + conditional rendering in RecipeNavbar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import SignupPage from './pages/SignupPage/SignupPage';
 import RecipesPage from './pages/RecipesPage/RecipesPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipeNavbar from './components/RecipeNavbar/RecipeNavbar';
+import { useState } from 'react';
 
 
 function App() {
+  const [activeUser, setActiveUser] = useState({id: "123", fname: "Nir", lname: "Channes", email: "nir@nir.com"});
+  // const [activeUser, setActiveUser] = useState(null);
+
   return (
     <>
       <HashRouter>
         <Switch>
           <Route exact path="/" >
-            <RecipeNavbar/>
+            <RecipeNavbar activeUser={activeUser}/>
             <HomePage/>
           </Route>
           <Route exact path="/login" component={LoginPage}></Route>
--- src/components/RecipeNavbar/RecipeNavbar.js
+++ src/components/RecipeNavbar/RecipeNavbar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 
-function RecipeNavbar(props) {
+function RecipeNavbar({activeUser}) {
     return (
         <Navbar bg="light" expand="lg">
         <Navbar.Brand href="#/">Recipe Book</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="#/recipes">Recipes</Nav.Link>
+                {activeUser ? <Nav.Link href="#/recipes">Recipes</Nav.Link> : null}
             </Nav>
             <Nav className="ml-auto">
-                <Nav.Link href="#/login">Login</Nav.Link>
-                <Nav.Link href="#/signup">Signup</Nav.Link>
-                <Nav.Link href="#">Logout</Nav.Link>
+                {!activeUser ? <Nav.Link href="#/login">Login</Nav.Link> : null}
+                {!activeUser ? <Nav.Link href="#/signup">Signup</Nav.Link> : null}
+                {activeUser ? <Nav.Link href="#">Logout</Nav.Link> : null}
             </Nav>
         </Navbar.Collapse>
         </Navbar>    
